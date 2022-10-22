a4ca603cd5d06d09dcf27d6d4ef8beaf18d93362
Kurapov <mastix2p@gmail.com>
2022-10-22 17:39:44 +0300

    jdm slides

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 function slidesPlugin(activeSlides = 0) {
     const slides = document.querySelectorAll('.slide')
 
-<<<<<<< HEAD
     slides[activeSlides].classList.add('active')
     
     for (const slide of slides) {
@@ -19,16 +18,3 @@ function slidesPlugin(activeSlides = 0) {
 }
 
 slidesPlugin(3)
-=======
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActive()
-        slide.classList.add('active')
-    })
-}
-function clearActive() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
->>>>>>> 69ffcb0b4e723c1482c1b1147d8fb35e4e8401df
